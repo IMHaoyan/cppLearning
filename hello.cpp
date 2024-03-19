9848e63c9749597c74d1e2e41efd6be0ec4482a6
HaoyanGong <657290301@qq.com>
2024-03-19 15:13:15 +0800

    copy的用法

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
-#include<unordered_map>
+#include <iostream>
 #include <memory>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
-int main(){
-    unique_ptr<int> pInt(new int(5));
-    int a =1;
-    shared_ptr<int> p = make_shared<int>(1);
+
+int main() {
+    
+    vector<int> nums = {1,2,3,4,5,6};
+    for(int i : nums){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<endl;
+    vector<int> b(6);
+    copy(nums.begin(), nums.end(), b.begin());
+    vector<int>().swap(b);
+    for(int i : b){
+        cout<<i<<" ";
+    }
+    cout<<endl;
     return 0;
 }
